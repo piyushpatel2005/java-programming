247b931b67be1aa60d707eaf347d8d4a3301c6f8
pc <piyushpatel2005@gmail.com>
2017-06-12 18:22:26 -0400

    Setters and getters

---
+++ basics/ch02/bank-account/AccountTest.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+public class AccountTest {
+	public static void main(String[] args) {
+		Account account1 = new Account("Jane Green", 50.00);
+		Account account2 = new Account("Zamira Mardanova", -7.53);
+		
+		System.out.printf("%s balance: %.2f%n", account1.getName(), account1.getBalance());
+		System.out.printf("%s balance: %.2f%n", account2.getName(), account2.getBalance());
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter deposit amokunt for account1: ");
+		double depositAmount = 0;
+		depositAmount = input.nextDouble();
+		System.out.printf("%nadding %.2f to account1 balance%n%n", depositAmount);
+		account1.deposit(depositAmount);
+		
+		System.out.printf("%s balance: %.2f%n", account1.getName(), account1.getBalance());
+		System.out.printf("%s balance: %.2f%n", account2.getName(), account2.getBalance());
+		
+		System.out.print("Enter deposit amount for account2: ");
+		depositAmount = input.nextDouble();
+		System.out.printf("%nadding %.2f to account2 balance%n%n", depositAmount);
+		account2.deposit(depositAmount);
+		
+		System.out.printf("%s balance: %.2f%n", account2.getName(), account2.getBalance());
+	}
+}

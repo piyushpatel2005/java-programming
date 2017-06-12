a0b782259f31bda0fc53482545adcd834a7f8557
pc <piyushpatel2005@gmail.com>
2017-06-11 23:14:56 -0400

    add constructor

--- basics/ch02/gradebook2/GradeBook.java
+++ basics/ch02/gradebook2/GradeBook.java
@@ -2,6 +2,10 @@ public class GradeBook {
 
 	private String courseName;
 	
+	public GradeBook(String name) {
+		courseName = name;
+	}
+	
 	public void setCourseName(String name) {
 		courseName = name;
 	}
--- basics/ch02/gradebook2/GradeBookTest.java
+++ basics/ch02/gradebook2/GradeBookTest.java
@@ -5,15 +5,10 @@ public class GradeBookTest {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		GradeBook myGradeBook = new GradeBook();
+		GradeBook gradeBook1 = new GradeBook("CS 101 Intro to Java Programming");
+		GradeBook gradeBook2 = new GradeBook("CS 102 Data Structures in Java");
 		
-		System.out.printf("Initial course name is : %s%n%n", myGradeBook.getCourseName());
-		
-		System.out.println("Please, enter the course name: ");
-		String theName = input.nextLine();
-		myGradeBook.setCourseName(theName);
-		System.out.println();
-		
-		myGradeBook.displayMessage();
+		System.out.printf("gradebook1 course name is %s%n", gradeBook1.getCourseName());
+		System.out.printf("gradeBook2 course name is %s%n", gradeBook2.getCourseName());
 	}
 }

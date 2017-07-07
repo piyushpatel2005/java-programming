727a5eb0ed89c50fe2fb1803a3105c99086103f3
Piyush Patel <piyushpatel2005@gmail.com>
2017-07-06 23:10:35 -0400

    Sorting and searching algorithms

---
+++ advanced/19-sort-search/LinearSearch.java
@@ -0,0 +1,42 @@
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class LinearSearch {
+  public static int linearSearch(int data[], int searchKey) {
+    for(int i = 0; i < data.length; i++) {
+      if(data[i] == searchKey)
+        return i;
+    }
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    SecureRandom generator = new SecureRandom();
+
+    int[] data = new int[10];
+
+    for(int i = 0; i < data.length; i++) {
+      data[i] = 10 + generator.nextInt(90);
+    }
+
+    System.out.printf("%s%n%n", Arrays.toString(data));
+
+    System.out.print("Please enter an integer value (-1 to quit): ");
+    int searchInt = input.nextInt();
+
+    while(searchInt != -1) {
+      int position = linearSearch(data, searchInt);
+
+      if(position == -1)
+        System.out.printf("%d was not found%n%n", searchInt);
+      else
+        System.out.printf("%d was found in position %d%n%n", searchInt, position);
+
+      System.out.print("Please enter an integer value (-1 to quit): ");
+      searchInt = input.nextInt();
+    }
+  }
+}

bc3b787722273e0ae721650d192adb7c7b24331c
Piyush Patel <piyush.patel@bell.ca>
2020-03-20 19:23:28 -0400

    Lightbend Futures course starting code

--- workspace/Lightbend-Java-Language-Lambdas/exercises/src/main/java/com/lightbend/lambdas/MessageTransformer.java
+++
@@ -1,6 +0,0 @@
-package com.lightbend.lambdas;
-
-@FunctionalInterface
-interface MessageTransformer<T> {
-    T apply(Message message);
-}
--- workspace/Lightbend-Java-Language-Lambdas/exercises/src/main/java/com/lightbend/lambdas/MessageTransformers.java
+++ workspace/Lightbend-Java-Language-Lambdas/exercises/src/main/java/com/lightbend/lambdas/MessageTransformers.java
@@ -6,40 +6,17 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 class MessageTransformers {
-    static MessageTransformer<String> toString = new MessageTransformer<String>() {
-        @Override
-        public String apply(Message message) {
-            return message.toString();
-        }
-    };
+    static Function<Message, String> toString = Message::toString;
 
-    static MessageTransformer<Instant> toTimestamp = new MessageTransformer<Instant>() {
-        @Override
-        public Instant apply(Message message) {
-            return message.getTimestamp();
-        }
-    };
+    static Function<Message, Instant> toTimestamp = Message::getTimestamp;
 
-    static MessageTransformer<Message> toUpperCase = new MessageTransformer<Message>() {
-        @Override
-        public Message apply(Message message) {
-            return new Message(message.getId(), message.getTimestamp(), message.getContent().toUpperCase());
-        }
-    };
+    static Function<Message, Message> toUpperCase = message -> new Message(message.getId(), message.getTimestamp(), message.getContent().toUpperCase());
 
-    static <T> List<T> mapMessages(List<Message> messages, MessageTransformer<T> transformer) {
+    static <T> List<T> mapMessages(List<Message> messages, Function<Message, T> transformer) {
         return messages
                 .stream()
-                .map(transformFunction(transformer))
+                .map(transformer)
                 .collect(Collectors.toList());
     }
 
-    private static <T> Function<Message, T> transformFunction(MessageTransformer<T> transformer) {
-        return new Function<Message, T>() {
-            @Override
-            public T apply(Message message) {
-                return transformer.apply(message);
-            }
-        };
-    }
 }
